dbef7f346dd6c48eefa557a5d7d2c7d78d61d33e
Laxeros07 <you@example.com>
2022-04-25 15:32:40 +0200

    final

--- Übung01/location.js
+++ Übung01/location.js
@@ -1,12 +1,22 @@
+/**
+ * DOM-Manipulation Titel
+ */
 document.title = "Uebung2";
-
+/**
+ * DOM-Manipulation Metadaten
+ */
 var metadaten = document.createElement("meta");
 metadaten.setAttribute("name", "author");
 metadaten.setAttribute("content", "Anne Staskiewicz");
-
+/**
+ * Speicherung von dem HTML-Element als Variable
+ */
 let element = document.getElementById("standort");
 
 var x = document.getElementById("demo");
+/**
+ * Funktion zur Standortermittlung
+ */
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(showPosition);
@@ -14,6 +24,9 @@ function getLocation() {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
+/**
+ * speichert longitude und latitude
+ */
 function showPosition(position) {
   x.innerHTML =
     "Latitude: " +
@@ -24,6 +37,7 @@ function showPosition(position) {
   let input = document.getElementById("textfeld");
 
   input.value = JSON.stringify({
+    // wandelt Coordinaten des Standorts in String in JSON um
     type: "Feature",
     properties: {
       shape: "Marker",
@@ -36,23 +50,28 @@ function showPosition(position) {
     },
   });
 }
+/**
+ * berechnet bzw sollte den Abstand zwischen meinem Standort und den POI berechnen
+ */
 function berechneAbstand() {
   var poiCoordinates = [];
-
+  //tut die Koordinaten von den poi in einen array
   poi.features.forEach((element) => {
     poiCoordinates.push(element.geometry.coordinates);
   });
+  //wandelt die Standortkoordinaten in einen Array um
   var standort = JSON.parse(textfeld.value);
   var standortCoordinates = standort.geometry.coordinates;
-
+  //berechnet den Abstand zw. dem Standort und den poi Koordinaten
   var abstand = [];
   poiCoordinates.forEach((element) => {
     abstand.push(berechneDistanz(element, standortCoordinates));
   });
-
+  //sortiert die Abstaende nach Groeße
   abstand.sort(function (a, b) {
     return a - b;
   });
+  //abstaende auf der website ausgeben
   document.getElementById("abstandPOI").innerHTML = "";
   abstand.forEach((element) => {
     document.getElementById("abstandPOI").innerHTML += "<p>" + element + "</p>";
